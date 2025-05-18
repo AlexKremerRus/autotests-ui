1f50b5458a637e5b61590d4f421fee4977df8b30
Alexey Nekhoroshev <nekhorosheff.a.v@yandex.ru>
2025-05-18 21:08:30 +0300

    Введение в Playwright.Практика с Playwright

--- playwright_registration.py
+++ playwright_registration.py
@@ -19,4 +19,4 @@ with sync_playwright() as playwright:
     dashboard_text = page.get_by_test_id('dashboard-toolbar-title-text')
     expect(dashboard_text).to_have_text('Dashboard')
 
-    # page.wait_for_timeout(1000)
+

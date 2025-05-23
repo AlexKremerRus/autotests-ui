1d0b4c129e428961ba0caa38e4e4dc6a61f61bd3
Alexey Nekhoroshev <nekhorosheff.a.v@yandex.ru>
2025-05-23 22:33:32 +0300

    Введенив Pytest

---
+++ tests/test_pytest.py
@@ -0,0 +1,16 @@
+def test_test():
+    print("sd")
+
+
+class TestClass:
+    def test_1(self):
+        pass
+
+    def test_2(self):
+        pass
+
+    def test_assert_positive(self):
+        assert 2+2 == 4
+
+    def test_assert_negative(self):
+        assert 2+2 == 7, "2+2 != 7 "

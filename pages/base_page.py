b5958fde208285e471ecad53a570abee8036430a
Alexey Nekhoroshev <nekhorosheff.a.v@yandex.ru>
2025-05-29 20:58:57 +0300

    Знакомство с PageObject

---
+++ pages/base_page.py
@@ -0,0 +1,14 @@
+# /pages/base_page.py
+
+from playwright.sync_api import Page
+
+
+class BasePage:
+    def __init__(self, page: Page):
+        self.page = page
+
+    def visit(self, url: str):
+        self.page.goto(url, wait_until='networkidle')  # у меня сомнения на то что нужно дожидаться всех нетворков
+
+    def reload(self):
+        self.page.reload(wait_until='domcontentloaded')
